5758d356adf1e24f2acbeecea0af0bfd77a86843
Larry Hill <tomella1@gmail.com>
2021-09-12 14:26:24 +0930

    Use fetch API instead of xml http request and add async stuff

--- web/lib/loader.js
+++ web/lib/loader.js
@@ -1,24 +1,5 @@
-const _CACHE = {};
-
-export default function(url, cacheable) {
-   return new Promise((resolve, reject) => {
-      if(cacheable && _CACHE[url]) {
-         resolve(_CACHE[url]);
-         return;
-      }
-
-      let xhr = new XMLHttpRequest();
-      xhr.open('GET', url);
-      xhr.setRequestHeader('Content-Type', 'application/json');
-      xhr.responseType = 'json';
-      xhr.onload = () => {
-         if (xhr.status !== 200) return;
-         let response = xhr.response;
-         if(cacheable) {
-            _CACHE[url] = response;
-         }
-         resolve(response);
-      };
-      xhr.send();
-   });
+export default async function (url) {
+   let fetcher = await fetch(url);
+   let response = await fetcher.json();
+   return response;
 }
