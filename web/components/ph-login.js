43906d56a52e1ebaffd35a62f1330685303f8f58
Larry Hill <tomella1@gmail.com>
2021-09-20 17:14:34 +0930

    Add the local signin to page

---
+++ web/components/ph-login.js
@@ -0,0 +1,95 @@
+
+const template = document.createElement('template');
+
+template.innerHTML = `
+<style>
+form {
+   width: 265px;
+   background-color: white;
+   border:  2px solid rgb(0,0,0,0.2);
+   border-radius: 6px;
+   padding: 6px;
+   line-height: 1.6;
+}
+label {
+   display: inline-block;
+   width: 6em;
+   font-weight: bold;
+}
+button {
+   margin-top: 6px;
+   padding: 5px;
+   font-weight: bolder;
+   border: 2px solid rgb(0,0,0,0.2);
+   border-radius: 4px;
+}
+</style>
+<div class="ph-login">
+   <button class="ph-login-show">Login...</button>
+   <form action="/login" method="post" hidden="hidden">
+      <label for="username">User Name</label><input type="text" name="username" placeholder="Username">
+      <br/>
+      <label for="username">Password</label><input type="password" name="password" placeholder="Password">
+      <br/>
+      <button class="ph-cancel">Cancel</button>
+      <button style="float: right" type="submit" class="ph-submit" disabled="disabled">Sign in</button>
+   </form>
+</div>
+`;
+
+customElements.define('ph-login', class LoginElement extends HTMLElement {
+
+   $(selector) {
+      return this.shadowRoot && this.shadowRoot.querySelector(selector)
+   }
+
+   $$(selector) {
+      return this.shadowRoot && this.shadowRoot.querySelectorAll(selector)
+   }
+
+   constructor() {
+      super();
+      // Normally you are adding the template
+      const root = this.attachShadow({ mode: 'open' })
+      root.appendChild(template.content.cloneNode(true));
+   }
+
+   connectedCallback() {
+      const form = this.$("form");
+      const show = this.$(".ph-login-show");
+      const cancel = this.$(".ph-cancel");
+      const submit = this.$(".ph-submit");
+      const username = this.$("[name='username']");
+      const password = this.$("[name='password']");
+
+      //this.shadowRoot.addEventListener('jobexpand', (e) => console.log(e));
+      show.addEventListener("click", ev => {
+         show.setAttribute("hidden", "hidden");
+         form.removeAttribute("hidden");
+      });
+
+      cancel.addEventListener("click", ev => {
+         form.setAttribute("hidden", "hidden");
+         show.removeAttribute("hidden");
+         ev.preventDefault();
+         return false;
+      });
+
+      username.addEventListener("input", ev => {
+         if(username.value && password.value) {
+            submit.removeAttribute("disabled");
+         } else {
+            submit.setAttribute("disabled", "disabled");
+         }
+
+      });
+      password.addEventListener("input", ev => {
+         if(username.value && password.value) {
+            submit.removeAttribute("disabled");
+         } else {
+            submit.setAttribute("disabled", "disabled");
+         }
+      });
+   }
+
+});
