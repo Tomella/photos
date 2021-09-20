8b471d667aea474745084968510863a2b4dac0b4
Larry Hill <tomella1@gmail.com>
2021-09-20 17:15:39 +0930

    Update server to reflect latest login changes.

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 import express from "express";
+import bodyParser from "body-parser";
 import config from "./lib/config.js";
 import ejs from "ejs";
 import mysql from "mysql2/promise";
 import passport from "passport";
-import passportFacebook from 'passport-facebook';
+
 import session from "express-session";
-import {spawn} from "child_process";
+import { spawn } from "child_process";
 
 import Photo from "./lib/photo.js";
-import Security from "./lib/security.js";
+import Security from "./lib/usersecurity.js";
 import StaticMapper from "./lib/staticmapper.js";
 import DevSecurity from "./lib/dev-security.js";
 import Thumb from "./lib/thumb.js";
@@ -25,217 +26,196 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-let security = null;
-if(config.isLocal.on) {
-   security = new DevSecurity(config);
-} else {
-   security = new Security(config);
-}
-
-
-const FacebookStrategy = passportFacebook.Strategy;
 const port = config.port;
 
-run().then(() => console.log("Running"));
 
-async function run() {
-   const pool = mysql.createPool(config.connection);
-   const photo = new Photo(pool);
-   const user = new User(pool);
-   const thumb = new Thumb(config.server);
+const pool = mysql.createPool(config.connection);
+const photo = new Photo(pool);
+const user = new User(pool);
+let security = null;
 
-   const keywordsRouter = new KeywordsRouter(pool);
-   const albumRouter = new AlbumRouter(pool);
 
-   let photos = await photo.all();
+const thumb = new Thumb(config.server);
 
-   //var httpProxy = require('http-proxy');
-   let app = express();
-   app.set('view engine', 'ejs');
-   app.engine('html', ejs.renderFile);
+const keywordsRouter = new KeywordsRouter(pool);
+const albumRouter = new AlbumRouter(pool);
 
-   // serve static files
+let photos = await photo.all();
 
-   app.use(express.static("web"));
-   app.use(express.static("photos"));
-   app.use('/src', express.static('src'));
+//var httpProxy = require('http-proxy');
+let app = express();
+app.set('view engine', 'ejs');
+app.engine('html', ejs.renderFile);
+app.use(bodyParser.urlencoded({ extended: false }))
+// serve static files
 
-   app.use('/keywords', keywordsRouter.router);
-   app.use('/albumService', albumRouter.router);
+app.use(express.static("web"));
+app.use(express.static("photos"));
+app.use('/src', express.static('src'));
 
-   app.use(session({
-      resave: false,
-      saveUninitialized: true,
-      secret: 'SECRET'
-   }));
+app.use('/keywords', keywordsRouter.router);
+app.use('/albumService', albumRouter.router);
 
-   app.use(passport.initialize());
-   app.use(passport.session());
+app.use(session({
+   resave: false,
+   saveUninitialized: true,
+   secret: 'SECRET'
+}));
 
-   passport.serializeUser(function (user, cb) {
-      cb(null, user);
-   });
+app.use(passport.initialize());
+app.use(passport.session());
 
-   passport.deserializeUser(function (obj, cb) {
-      cb(null, obj);
-   });
+passport.serializeUser(function (user, cb) {
+   console.log("LUSER1", user)
+   cb(null, user);
+});
 
-   console.log("Auth", config.auth)
+passport.deserializeUser(function (obj, cb) {
+   console.log("LUSER2", obj)
+   cb(null, obj);
+});
 
-   passport.use(new FacebookStrategy({
-      clientID: config.auth.clientID,
-      clientSecret: config.auth.clientSecret,
-      callbackURL: config.auth.callbackURL,
-      failureFlash: true,
-      profileFields: ["email", "name"]
-   },
-      async function (accessToken, refreshToken, profile, cb) {
-         console.log("(accessToken, refreshToken, profile, cb", accessToken, refreshToken, profile);
-         let client = await user.findCreate(profile._json);
-         profile.admin = client.admin;
-         cb(null, profile);
-      }
-   ));
-
-   app.all('/download/:name', function(req, res) {
-      let path = __dirname + config.processDownload.photosDirectory + sanitise(req.params.name);
-      console.log("PATH = " + path)
-      res.download(path);
-   });
-
-   app.get('/auth/facebook', passport.authenticate('facebook', {
-      scope: ['public_profile', 'email']
-   }));
-
-   app.get('/callback/facebook',
-      passport.authenticate('facebook', {
-         successRedirect: '/',
-         failureRedirect: '/error'
-      })
-   );
-
-   // It has to be down here to pick up the authentication.
-   const photoRouter = new PhotoRouter(config.server, photo, app, [isAdmin]);
-   const staticMapper = new StaticMapper(app, "/scripts/", __dirname + '/node_modules/', config.staticMappings);
-
-   app.all('/all', async (req, res) => {
-      let photos = await photo.all(req.query.startDate, req.query.endDate);
-      let response = pointsToJson(photos);
-      if(req.user) {
-         response = Object.assign(response, { properties: {user: req.user.name, admin: req.user.admin}});
-      }
-      res.status(200).send(response);
-   });
-
-   app.all('/user.js', async (req, res) => {
-      let response = security.user(req);
-      let str = "export default " + JSON.stringify(response);
-      res.setHeader('Content-Type', "application/javascript; charset=UTF-8");
-      res.send(str);
-   });
-
-   app.all('/error', async (req, res) => {
-      res.json({success: false});
-   });
-
-   app.all('/success', async (req, res) => {
-      let raw = req.user._json;
-
-      let data = await user.findCreate(raw);
-      res.json(data);
-   });
-
-   app.get('/logout', async (req, res) => {
-      req.logout();
-      res.redirect('/');
-   });
-
-   app.all('/clusters', async (req, res) => {
-      console.log(req.query);
-      let zoom = (+req.query.zoom) ? req.query.zoom : 18;
-      let request = new ExtentRequest(-179, 179, -89, 89, zoom);
-      let response = await queryingService.getSummary(request);
-      res.status(200).send(response);
-   });
-
-   app.all('/edit/:id', isAdmin, async (req, res) => {
-      let id = req.params.id;
-      let data = await photo.findByIdWithAdjacent(id);
-      if(!data) {
-         data = await photo.findNext(id);
-         if(!data) {
-            data = await photo.findPrevious(id);
-         }
-
-         if(!data) {
-            res.redirect('/');
-         } else {
-            id = data.id;
-            res.redirect('/edit/' + id);
-         }
-      } else {
-         ejs.renderFile(__dirname + "/views/edit.ejs", { data: JSON.stringify(data) }, {}, function (err, str) {
-            if (err) {
-               console.log(err)
-               res.writeHead(503, 'System error');
-               res.end()
-            } else {
-               res.status(200).send(str);
-            }
-         });
-      }
-   });
-
-   // Direction can be right, left, 180 or original. All are relative to the original photo.
-   app.all('/rotatethumbOld/:direction', isAdmin, async (req, res) => {
-      try {
-         let data = await photo.findById(req.query.id);
-         await thumb.rotateThumb(data.filename, req.params.direction);
-         res.send(data);
-      } catch(e) {
-         res.send(e);
-      }
-   });
-
-   // Direction can be right, left, 180 or original. All are relative to the original photo.
-   app.all('/rotatethumb/:direction', isAdmin, async (req, res, next) => {
-      let data = await photo.findById(req.query.id);
-
-      let subprocess = spawn("node", ['lib/processrotate.js', '--filename=' + data.filename, '--direction=' + req.params.direction]);
-
-      for await (const chunk of subprocess.stdout) {
-          console.log('stdout chunk: ' + chunk);
-      }
-
-      let code = await new Promise( (resolve, reject) => {
-         subprocess.on('close', resolve);
-      });
-      if(code === 0) {
-         res.send(data);
-      } else {
-         res.json({success: !code});;
-      }
-   });
-
-   // Direction can be right, left, 180 or original. All are relative to the original photo.
-   app.all('/saveannotation/:id', isAdmin, async (req, res) => {
-      try {
-         let data = await photo.findById(req.params.id);
-         data.annotation = req.query.annotation;
-         await photo.update(data);
-         res.send(data);
-      } catch(e) {
-         res.send(e);
-      }
-   });
-
-   app.listen(port, function (err) {
-      console.log("running server on port " + port);
-   });
+if (config.isLocal.on) {
+   security = new DevSecurity(config);
+} else {
+   security = new Security(user, passport);
 }
 
+
+app.post('/login', passport.authenticate('local', { failureRedirect: '/?success=false' }),
+   function (req, res) {
+      res.redirect('/?success=true');
+   }
+);
+
+app.all('/logout', async (req, res) => {
+   req.logout();
+   res.redirect('/');
+});
+
+app.all('/download/:name', function (req, res) {
+   let path = __dirname + config.processDownload.photosDirectory + sanitise(req.params.name);
+   console.log("PATH = " + path)
+   res.download(path);
+});
+
+// It has to be down here to pick up the authentication.
+const photoRouter = new PhotoRouter(config.server, photo, app, [isAdmin]);
+const staticMapper = new StaticMapper(app, "/scripts/", __dirname + '/node_modules/', config.staticMappings);
+
+app.all('/all', async (req, res) => {
+   let photos = await photo.all(req.query.startDate, req.query.endDate);
+   let response = pointsToJson(photos);
+   if (req.user) {
+      response = Object.assign(response, { properties: { user: req.user.name, admin: req.user.admin } });
+   }
+   res.status(200).send(response);
+});
+
+app.all('/user.js', async (req, res) => {
+   let response = security.user(req);
+   let str = "export default " + JSON.stringify(response);
+   res.setHeader('Content-Type', "application/javascript; charset=UTF-8");
+   res.send(str);
+});
+
+app.all('/error', async (req, res) => {
+   res.json({ success: false });
+});
+
+app.all('/success', async (req, res) => {
+   let raw = req.user._json;
+
+   let data = await user.findCreate(raw);
+   res.json(data);
+});
+
+app.all('/clusters', async (req, res) => {
+   console.log(req.query);
+   let zoom = (+req.query.zoom) ? req.query.zoom : 18;
+   let request = new ExtentRequest(-179, 179, -89, 89, zoom);
+   let response = await queryingService.getSummary(request);
+   res.status(200).send(response);
+});
+
+app.all('/edit/:id', isAdmin, async (req, res) => {
+   let id = req.params.id;
+   let data = await photo.findByIdWithAdjacent(id);
+   if (!data) {
+      data = await photo.findNext(id);
+      if (!data) {
+         data = await photo.findPrevious(id);
+      }
+
+      if (!data) {
+         res.redirect('/');
+      } else {
+         id = data.id;
+         res.redirect('/edit/' + id);
+      }
+   } else {
+      ejs.renderFile(__dirname + "/views/edit.ejs", { data: JSON.stringify(data) }, {}, function (err, str) {
+         if (err) {
+            console.log(err)
+            res.writeHead(503, 'System error');
+            res.end()
+         } else {
+            res.status(200).send(str);
+         }
+      });
+   }
+});
+
+// Direction can be right, left, 180 or original. All are relative to the original photo.
+app.all('/rotatethumbOld/:direction', isAdmin, async (req, res) => {
+   try {
+      let data = await photo.findById(req.query.id);
+      await thumb.rotateThumb(data.filename, req.params.direction);
+      res.send(data);
+   } catch (e) {
+      res.send(e);
+   }
+});
+
+// Direction can be right, left, 180 or original. All are relative to the original photo.
+app.all('/rotatethumb/:direction', isAdmin, async (req, res, next) => {
+   let data = await photo.findById(req.query.id);
+
+   let subprocess = spawn("node", ['lib/processrotate.js', '--filename=' + data.filename, '--direction=' + req.params.direction]);
+
+   for await (const chunk of subprocess.stdout) {
+      console.log('stdout chunk: ' + chunk);
+   }
+
+   let code = await new Promise((resolve, reject) => {
+      subprocess.on('close', resolve);
+   });
+   if (code === 0) {
+      res.send(data);
+   } else {
+      res.json({ success: !code });;
+   }
+});
+
+// Direction can be right, left, 180 or original. All are relative to the original photo.
+app.all('/saveannotation/:id', isAdmin, async (req, res) => {
+   try {
+      let data = await photo.findById(req.params.id);
+      data.annotation = req.query.annotation;
+      await photo.update(data);
+      res.send(data);
+   } catch (e) {
+      res.send(e);
+   }
+});
+
+app.listen(port, function (err) {
+   console.log("running server on port " + port);
+});
+
+
 function isAdmin(req, res, next) {
-   if(security.isAdmin(req)) {
+   if (security.isAdmin(req)) {
       return next();
    }
    res.redirect('/');
@@ -269,5 +249,5 @@ function pointsToJson(photos, name = "photos") {
 
 function sanitise(input) {
    // Just wipe out multiple full stops, slashes and backslashes at the moment.
-   return (input? input :"").replace(/\\|\/|\.{2,}/gi, "");
+   return (input ? input : "").replace(/\\|\/|\.{2,}/gi, "");
 }
