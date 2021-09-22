656e8c00f59abd06cb0fce78ed493e652db874da
Larry Hill <tomella1@gmail.com>
2021-09-22 16:56:08 +0930

    Get rid of facebook js handler

--- web/app/app.js
+++ web/app/app.js
@@ -185,7 +185,14 @@ function clusterOut(event) {
 }
 
 function loggedIn(event) {
-    phThumbLink.setAttribute("edit", event.detail.admin);
+   let detail = event.detail;
+    phThumbLink.setAttribute("edit", detail.admin);
+    
+    let user = detail.name;
+    console.log('Successful login for: ' + user.givenName);
+    document.getElementById('nameDiv').hidden = false;
+    document.getElementById('loginDiv').hidden = true;
+    document.getElementById('nameTarget').innerHTML = user.givenName + " " + user.familyName;
 }
 
 function changeDates(event) {
