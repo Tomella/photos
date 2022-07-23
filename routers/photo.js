0b33a24d12c2a810aedc645edd56102d93f61130
Tomella <tomella1@gmail.com>
2022-07-24 02:00:15 +0930

    Record result of spawned job.

--- routers/photo.js
+++ routers/photo.js
@@ -46,9 +46,9 @@ class PhotoRouter {
 
         //   node lib/processupload.js --file=uploads/1b9a795b-fe12-4a99-9a70-54e938944f37.json
         app.post('/upload', ...chain, upload.array('file', 120), async (req, res, next) => {
+            console.log("Starting upload");
             console.log(req.files);
 
-            console.log(req.files);
             let latitude = req.query.latitude;
             let longitude = req.query.longitude;
             let jobId = uuidv4();
@@ -56,53 +56,24 @@ class PhotoRouter {
             // Do we overwrite or set lat/lng?
             if(typeof latitude != "undefined" && typeof longitude != "undefined") {
                 req.files.forEach(element => {
-                    element.latitude = latitude;
-                    element.longitude = longitude;
+                    element.latitude = +latitude;
+                    element.longitude = +longitude;
                 });
             }
             let handle = await fs.writeFile("uploads/" + jobId + ".json", JSON.stringify(req.files));
 
             console.log(req.query, typeof latitude, typeof longitude);
 
-           //const subprocess = spawn("node", ['lib/processupload.js', '--file=uploads/' + jobId + '.json'], {
-            //    detached: true,
-            //    stdio: 'ignore'
-            //});
-
-            //subprocess.unref();
-            res.send({ jobId });
-            // req.files is array of `photos` files
-            // req.body will contain the text fields, if there were any
-        });
-
-        //   node lib/processupload.js --file=uploads/1b9a795b-fe12-4a99-9a70-54e938944f37.json
-        app.post('/uploadTest',  upload.array('file', 120), async (req, res, next) => {
-            console.log(req.files);
-            let latitude = req.query.latitude;
-            let longitude = req.query.longitude;
-            let jobId = uuidv4();
-
-            console.log(req.query, typeof latitude, typeof longitude);
-
-            let args = ['lib/processupload.js', '--file=uploads/' + jobId + '.json']
-
-            if(typeof latitude != "undefined" && typeof longitude != "undefined") {
-                args.push("--latitude=" + latitude);
-                args.push("--longitude=" + longitude);
-            }
-            const subprocess = spawn("node", args, {
+            const subprocess = spawn("node", ['lib/processupload.js', '--jobid=' + jobId], {
                 detached: true,
                 stdio: 'ignore'
             });
 
             subprocess.unref();
-            console.log(args);
-
             res.send({ jobId });
             // req.files is array of `photos` files
             // req.body will contain the text fields, if there were any
         });
-
     }
 }
 
